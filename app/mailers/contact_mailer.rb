b62a60279e60e17236564ac723a44c2caba90520
Daniel Mickleburgh <danielmickleburgh@hotmail.co.uk>
2018-02-26 19:13:38 +0000

    Added contact form mailer file

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,11 @@
+class ContactMailer < AcionMailer::Base
+  default to: 'danielmickleburgh@hotmail.co.uk'
+  
+  def contact_email(name, email, body)
+    @name = name
+    @email = email
+    @body = body
+    
+    mail(from: email, subject: 'Contact Form Message')
+  end
+end
